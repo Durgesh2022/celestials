344cddf145b52031e488fab591ece3f8dda0ee2e
Durgesh Tiwari <twdurgesh226@gmail.com>
2025-07-07 00:14:51 +0530

    Durgesh commit

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,19 @@
+import { Metadata } from "next";
 import Image from "next/image";
 import { Product } from "../../types/product";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+// Dynamic metadata (optional)
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return {
+    title: `Product ${params.id}`,
+  };
+}
 
 // Fetch product
 async function getProduct(id: string): Promise<Product | null> {
@@ -17,23 +31,19 @@ async function getProduct(id: string): Promise<Product | null> {
   }
 }
 
-// ✅ Dynamic route page
-export default async function ProductPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function ProductPage({ params }: Props) {
   const product = await getProduct(params.id);
 
   if (!product) {
     return <div>Product not found</div>;
   }
+
   return (
     <div className="relative min-h-screen bg-gradient-to-b from-[#f6cf92] to-white overflow-hidden">
-      <div className="min-h-screen p-6 ">
+      <div className="min-h-screen p-6">
         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
           {/* Left side: images */}
-          <div className="flex flex-row  space-x-12">
+          <div className="flex flex-row space-x-12">
             <div className="flex flex-col space-y-3">
               <Image
                 src={product.imageUrls[0]}
